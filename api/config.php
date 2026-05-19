c4b9cee0e250f7b2bfcacaeb94001008b4a0f5d0
Sai Nikhil Alahari <sainikil@Sais-MacBook-Pro.local>
2026-05-19 15:25:06 +0530

    Initialized Backend

---
+++ api/config.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_HOST', ' ');
+define('DB_NAME', ' ');
+define('DB_USER', ' ');
+define('DB_PASS', ' ');
+
+define('TAX_RATE', 0.18);
+define('FREE_DELIVERY_THRESHOLD', 1500);
+define('DELIVERY_CHARGE', 50);
+
+ini_set('display_errors', 0);
+error_reporting(0);
+
+session_start();
